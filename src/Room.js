d79297cf866eb8f44788e2177214e93621b7f8ce
Gregory Benison <gregbenison@fb.com>
2023-05-16 16:36:55 -0700

    Modals associated with each room

--- src/Room.js
+++ src/Room.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Room(props) {
-  return (
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    return (
     <>
-      <div className="room" style={{top: props.y + "px", left: props.x + "px"}} />
-      <RoomCard />
+      <div className="room" style={{top: props.y + "px", left: props.x + "px"}} onClick={handleShow}/>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Stuff you should know about this room</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            Lots of air changes going on in here
+        </Modal.Body>
+        <Modal.Footer>
+            <Button onClick={handleClose}>Turn On Purifiers</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
-}
-
-function RoomCard() {
-    return <div className="roomcard">A room a room a room</div>;
 }
 
 export default Room;
