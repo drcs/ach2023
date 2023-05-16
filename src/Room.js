2c95c084b9d67f80b8ca86456a4426862a5792b5
Gregory Benison <gregbenison@fb.com>
2023-05-15 21:00:38 -0700

    Add some interactivity to the rooms

--- src/Room.js
+++ src/Room.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Room(props) {
   return (
-    <div className="room" style={{top: props.y + "px", left: props.x + "px"}}>
-    </div>
+    <>
+      <div className="room" style={{top: props.y + "px", left: props.x + "px"}} />
+      <RoomCard />
+    </>
   );
+}
+
+function RoomCard() {
+    return <div className="roomcard">A room a room a room</div>;
 }
 
 export default Room;
