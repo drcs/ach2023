902b261cc5584fa20d9ba8d3a6af44e244325b05
Gregory Benison <gregbenison@fb.com>
2023-05-16 17:32:35 -0700

    multi-phase modals

--- src/Room.js
+++ src/Room.js
@@ -5,9 +5,11 @@ import Modal from 'react-bootstrap/Modal';
 function Room(props) {
     const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [phase, setPhase] = useState(1);
+    const nextPhase = () => setPhase(phase + 1);
 
+    const handleClose = () => {setShow(false); setPhase(1); }
+    const handleShow = () => {setShow(true); setPhase(1); }
 
     return (
     <>
@@ -16,15 +18,26 @@ function Room(props) {
         <Modal.Header closeButton>
           <Modal.Title>Stuff you should know about {props.roomName}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-            {props.ach} air changes per hour going on in {props.roomName}
-        </Modal.Body>
-        <Modal.Footer>
+            <Modal.Body>
+                <ModalBody phase={phase} ach={props.ach} roomName={props.roomName}/>
+            </Modal.Body>
+            <Modal.Footer>
             <Button onClick={handleClose}>Turn On Purifiers</Button>
+            <Button onClick={nextPhase}>Next</Button>
         </Modal.Footer>
       </Modal>
-    </>
-  );
+      </>
+      );
+    }
+
+function ModalBody(props) {
+    if (props.phase === 1) {
+      return <>{props.ach} air changes per hour going on in {props.roomName}</>;
+    } else if (props.phase === 2) {
+      return <>That's a lot of air changes</>;
+    } else {
+      return <>(the end)</>;
+    }
 }
 
 export default Room;
